19330ff6903ed463fc5a958d3865cb258342addf
Jed Darrohn <jamesdarrohn@gmail.com>
2015-06-19 15:16:05 -0400

    Removed Extra JS/CSS. Installed jQuery, Bootstrap, Font Awesome though Bower

--- index.php
+++ index.php
@@ -3,18 +3,17 @@
   
   <head>
   
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" >
   
   </head>
   
   <body>
+    
+    
 
   
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/stellar.js/0.6.2/jquery.stellar.min.js"></script>
+
     
   </body>
 
